71258d2d626a9cff9f9e62af15d532bbb2a87549
GalloIT <bgallay@palo-it.com>
2024-05-02 09:24:21 +0200

    Character can heal

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,18 @@ impl Character {
             alive: true,
         }
     }
+}
+
+impl Character {
+    pub(crate) fn heal(&self, healed: &mut Character, health: i32) {
+        if !healed.alive || healed.health == 1000 {
+            return;
+        }
+        healed.health = (healed.health + health).min(1000);
+    }
+}
+
+impl Character {
     pub(crate) fn deal_damage(&self, defender: &mut Character, damage: i32) {
         defender.health -= damage;
         if defender.health <= 0 {
@@ -53,6 +65,37 @@ mod tests {
             let mut other_character = Character::new();
             character.deal_damage(&mut other_character, 1000);
             assert_eq!(other_character.alive, false);
+        }
+    }
+
+    mod character_can_heal_a_character {
+        use super::*;
+
+        #[test]
+        fn when_a_character_heals_100_health_to_another_character_the_health_of_the_other_character_is_increased_by_100() {
+            let character = Character::new();
+            let mut other_character = Character::new();
+            character.deal_damage(&mut other_character, 200);
+            character.heal(&mut other_character, 100);
+            assert_eq!(other_character.health, 900);
+        }
+
+        #[test]
+        fn dead_character_cannot_be_healed() {
+            let character = Character::new();
+            let mut other_character = Character::new();
+            character.deal_damage(&mut other_character, 1000);
+            character.heal(&mut other_character, 100);
+            assert_eq!(other_character.health, 0);
+        }
+
+        #[test]
+        fn healing_cannot_increase_health_above_1000() {
+            let character = Character::new();
+            let mut other_character = Character::new();
+            character.deal_damage(&mut other_character, 10);
+            character.heal(&mut other_character, 100);
+            assert_eq!(other_character.health, 1000);
         }
     }
 }
