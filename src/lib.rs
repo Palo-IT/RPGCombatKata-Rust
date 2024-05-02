ddaa1fa45b50de5685c8c6d075993e5e6575833f
GalloIT <bgallay@palo-it.com>
2024-05-02 08:44:47 +0200

    Dealing damages

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,12 @@ struct Character {
 }
 
 impl Character {
+    pub(crate) fn deal_damage(&self, defender: &mut Character, damage: i32) {
+        defender.health -= damage;
+    }
+}
+
+impl Character {
     fn new() -> Self {
         Self {
             health: 1000,
@@ -13,7 +19,6 @@ impl Character {
         }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -25,5 +30,13 @@ mod tests {
         assert_eq!(character.health, 1000);
         assert_eq!(character.level, 1);
         assert_eq!(character.alive, true);
+    }
+
+    #[test]
+    fn when_a_character_deals_100_damage_to_another_character_the_health_of_the_other_character_is_reduced_by_100() {
+        let character = Character::new();
+        let mut other_character = Character::new();
+        character.deal_damage(&mut other_character, 100);
+        assert_eq!(other_character.health, 900);
     }
 }
